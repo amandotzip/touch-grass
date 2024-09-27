5956950c016156282058bf85b6ce5dee55b0427c
Amanuel Awoke <amanuelonline@gmail.com>
2024-09-26 22:40:56 -0400

    init

---
+++ backend/touchgrass/src/main/java/com/io/github/amandotzip/touchgrass/TouchgrassApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.amandotzip.touchgrass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouchgrassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouchgrassApplication.class, args);
+	}
+
+}
---
+++ backend/touchgrass/src/test/java/com/io/github/amandotzip/touchgrass/TouchgrassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.amandotzip.touchgrass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouchgrassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
